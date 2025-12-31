0f80017b68938df669ec8f8a8efe446b2524af8b
NellyNakhero <nelly.nakhero@gmail.com>
2025-12-31 18:16:19 +0300

    test commit and push

---
+++ Solidity/Fundamentals.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract Fundamentals {
+    //Variables: Allows storing of data in the program
+    //Types
+    //1. Address: Represents an ethereum address
+    address userAddress = 0x742d35Cc6634C0532925a3b844Bc454e4438f44e;
+    //2.Strings: Represents texts
+    string msg = "Hello world";
+    //3.Booleans: represents true or flase
+    bool isHappy = true;
+    //4.uint
+    uint age = 12;
+    uint256 public constant MAX_TIME = 10 days;
+}
