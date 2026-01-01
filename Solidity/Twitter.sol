ba0c506228f4f9d16be8dd221814189086d4b592
NellyNakhero <nelly.nakhero@gmail.com>
2026-01-01 14:34:17 +0300

    arrays in sol

--- Solidity/Twitter.sol
+++ Solidity/Twitter.sol
@@ -3,13 +3,31 @@ pragma solidity ^0.8.2;
 
 contract Twitter {
     //create mapping between buser and a tweet
-    mapping (address => string) public tweets;
+    mapping (address => string[]) public tweets;
+
+    string[] public actions = ["like", "reply", "reshare"];
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) public view returns (string memory) {
+    function getTweet(address _owner, uint _index) public view returns (string memory) {
+        return tweets[_owner][_index];
+    }
+
+    function getAllOwnerTweets(address _owner) public view returns (string[] memory) {
         return tweets[_owner];
+    }
+
+    function updateUserActionsOptions(string memory _action) public {
+        actions.push(_action);
+    }
+    
+    function getActionsLength() public view returns (uint) {
+        return actions.length;
+    }
+
+    function getAction(uint _index) public view returns (string memory) {
+        return actions[_index];
     }
 }
