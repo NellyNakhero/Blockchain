55b4af424dc763f973d3ea52d120f2d18d128196
NellyNakhero <nelly.nakhero@gmail.com>
2026-01-02 13:20:16 +0300

    struct in sol

--- Solidity/Twitter.sol
+++ Solidity/Twitter.sol
@@ -2,20 +2,33 @@
 pragma solidity ^0.8.2;
 
 contract Twitter {
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
     //create mapping between buser and a tweet
-    mapping (address => string[]) public tweets;
+    mapping (address => Tweet[]) public tweets;
 
     string[] public actions = ["like", "reply", "reshare"];
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+        Tweet memory newTweet = Tweet({
+            content : _tweet, 
+            timestamp: block.timestamp, 
+            author: msg.sender,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
     }
 
     function getTweet(address _owner, uint _index) public view returns (string memory) {
-        return tweets[_owner][_index];
+        return tweets[_owner][_index].content;
     }
 
-    function getAllOwnerTweets(address _owner) public view returns (string[] memory) {
+    function getAllOwnerTweets(address _owner) public view returns (Tweet[] memory) {
         return tweets[_owner];
     }
 
